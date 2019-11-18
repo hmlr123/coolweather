0caf4d37eef203132c3d2daa2b98432f2e68ab56
hmlr123 <2870027009@qq.com>
2019-11-18 22:05:57 +0800

    init repository

---
+++ app/src/main/java/com/hmlr123/coolweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hmlr123.coolweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
